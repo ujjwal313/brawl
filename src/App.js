fbf3d13e89b60431409c2b3be5b048ad001d5ea7
Ujjwal-Medpiper <intern7@medpiper.com>
2022-04-19 17:15:07 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = "https://api.brawlstars.com/v1/players/%23909lu288/battlelog";
+  let options = {
+    headers: {
+      Authorization:
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjIyM2M2NDBmLWJjOGEtNGIyOC05MDQwLWI5YTk3NGJiYjg2OSIsImlhdCI6MTY0OTc3OTExNywic3ViIjoiZGV2ZWxvcGVyL2ZhYWU1MGVkLWMwODAtMDZlZC1jMGE5LTFlMDAwODQ4NjAxZSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiNDkuMzcuNjkuMzIiXSwidHlwZSI6ImNsaWVudCJ9XX0.0m-FjPInZnO14zBoZWTUGiNFDLB64raxexIi2OkS7bIm_CNxKn0RCG3fKDijvD0xlvmtcedGlPUW3pFnT44tFg",
+      "Content-Type": "application/json",
+      // "Access-Control-Allow-Origin": "*",
+    },
+  };
+  useEffect(() => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
